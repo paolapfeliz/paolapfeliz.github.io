434adff2fa1302d77a85d864efc5b51c535c2303
Paola Feliz <pfeliz8316@gmail.com>
2023-12-29 18:36:41 -0600

    Initial Commit... Brenten was here

---
+++ script.js
@@ -0,0 +1,5 @@
+// Access the HTML element with the ID "helloWorldMessage"
+const helloWorldMessage = document.getElementById('helloWorldMessage');
+
+// Update the content of the element with the "Hello World" message
+helloWorldMessage.textContent = 'Hello World!';
